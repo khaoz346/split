14615c6834954f7df675d9e58d10471cb9bb71d6
Victor Wu <victor@plastiq.com>
2019-03-19 20:36:27 -0700

    Add new create offer form page and css, about to add drop down functionality on form

--- node.js
+++ node.js
@@ -1,16 +1,28 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 app.set('view engine', 'ejs');
 
 
 //Use static files e.g. css and js files/
 app.use(express.static("public"));
 
+//Use body parser with app
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get("/", (req,res) => {
   res.render('index');
 })
 
+app.get("/offer-form", (req,res) => {
+  res.render('offer-form');
+})
 
+app.post("/offer-form", (req,res) => {
+  let formData = req.body;
+  console.log(formData);
+  res.render('offer-form');
+})
 
 app.listen(3001, function(){
   console.log("Connected to port 3001.");
